bbdf50d5dd6ae483aaf848e11365b3b9f4eddaba
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2025-08-14 22:18:02 +0100

    Added build/processing of level data

--- build.sh
+++ build.sh
@@ -22,6 +22,59 @@ indexcat="${buildpath}/index.html"
 assetsrc="assets/tilemap_packed.png"
 assetcatsrc="assets/catmap.png"
 assetjs="tilemaps.js"
+leveljs="levels.js"
+
+# See if the levels asset need to be rebuilt
+mostrecentlevel=`ls -larth assets/level*.tmx | tail -1 | awk '{ print $NF }'`
+srcdate=`stat -c %Y ${mostrecentlevel} 2>/dev/null`
+destdate=`stat -c %Y ${leveljs} 2>/dev/null`
+
+# If no js asset found, force build
+if [ "${destdate}" == "" ]
+then
+  destdate=0
+fi
+
+# When source is newer, rebuild
+if [ ${srcdate} -gt ${destdate} ]
+then
+  echo -n "Rebuilding levels..."
+
+  # Clear old dest
+  echo -n "" > "${leveljs}"
+
+  # Start new file
+  echo -n "var levels=[" > "${leveljs}"
+  for file in assets/level*.tmx
+  do
+    echo -n "{" >> "${leveljs}"
+
+    for attrib in "width" "height"
+    do
+      echo -n "${attrib}:" >> "${leveljs}"
+      cat "${file}" | grep "<map " | tr ' ' '\n' | grep '^'${attrib}'=' | awk -F'"' '{ print $2 }' | tr -d '\n' >> "${leveljs}"
+      echo -n ',' >> "${leveljs}"
+    done
+
+#    for property in "title"
+#    do
+#      echo -n "${property}:\"" >> "${leveljs}"
+#      cat "${file}" | grep "<property " | grep 'name=\"'${property}'\"' | awk -F'"' '{ print $4 }' | tr -d '\n' >> "${leveljs}"
+#      echo -n '",' >> "${leveljs}"
+#    done
+
+    for assettype in "tiles" "chars"
+    do
+      echo -n "${assettype}:[" >> "${leveljs}"
+      cat "${file}" | tr -d '\n' | sed 's/<layer /\n<layer /g' | grep "${assettype}" | sed 's/</\n</g' | grep "<data " | awk -F'>' '{ print $2 }' | sed 's/,0,/,,/g' | sed 's/,0,/,,/g' | sed 's/^0,/,/g' | sed 's/,0$/,/g' | tr -d '\n' >> "${leveljs}"
+      echo -n "]," >> "${leveljs}"
+    done
+    echo -n "}," >> "${leveljs}"
+  done
+  echo -n "];" >> "${leveljs}"
+
+  echo "done"
+fi
 
 # See if the tilemaps asset needs to be rebuilt
 srcdatecat=`stat -c %Y ${assetcatsrc} 2>/dev/null`
@@ -74,7 +127,7 @@ mkdir "${buildpath}"
 # Concatenate the JS files
 echo "Concatenating JS"
 touch "${jscat}" >/dev/null 2>&1
-for file in "${assetjs}" "timeline.js" "main.js"
+for file in "${assetjs}" "${leveljs}" "timeline.js" "main.js"
 do
   cat "${file}" >> "${jscat}"
 done
